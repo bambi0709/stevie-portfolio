5d7970c2be46dfbc5c5717e110f910c6655f8af5
bambi0709 <kate.dinsdale1990@gmail.com>
2025-12-05 14:20:58 +0000

    Create main.py
    # My Guessing Game (Year 7)
    # I'm starting this project by setting up the file. 
    # I will write the full code for the game myself as I learn how to use Python.

---
+++ year7/guessing-game/main.py
@@ -0,0 +1,24 @@
+# My Guessing Game (Year 7)
+
+This is one of my first Python projects.  
+I’m building a game where the computer chooses a secret number and I have to guess it.
+
+## What I want my game to do
+- Pick a random number
+- Let me make guesses
+- Tell me if my guess is too high or too low
+- Tell me when I'm correct
+- Count how many guesses I used
+
+## Skills I will use or learn
+- Python input and output
+- Variables
+- Loops
+- If/else logic
+- Testing and debugging
+
+## What I learned
+(I will fill this in once the project is finished.)
+
+## Ideas to improve my game
+(I will add ideas here after I complete the first version.)
